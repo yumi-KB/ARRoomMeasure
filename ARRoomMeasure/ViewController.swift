52985247f7e69a54cc3d8bc2d2c58b8c7c27e104
yumi kanebayashi <bon.kyumi@gmail.com>
2021-01-15 00:34:51 +0900

    mission-4 オブジェクト間にラインを表示

--- ARRoomMeasure/ViewController.swift
+++ ARRoomMeasure/ViewController.swift
@@ -7,6 +7,7 @@ class ViewController: UIViewController {
     @IBOutlet var sceneView: ARSCNView!
     
     var dotNodes: [SCNNode] = []
+    var lineNodes: [SCNNode] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +25,6 @@ class ViewController: UIViewController {
         sceneView.session.run(configuration)
     }
     
-    @IBAction func UndoAction(_ sender: UIButton) {
-        if dotNodes == [] { return }
-        // sceneViewから削除
-        dotNodes.last?.removeFromParentNode()
-        // 配列から削除
-        dotNodes.removeLast()
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // ドット上限を20個に指定
         if dotNodes.count >= 20 {
@@ -41,59 +34,96 @@ class ViewController: UIViewController {
         guard let touch = touches.first else { return }
         // スクリーン座標に変換する
         let touchLocation = touch.location(in: sceneView)
-            // タップされた位置の特徴点にあるARアンカーを探す
-            let hitTestResults = sceneView.hitTest(touchLocation, types: .existingPlane)
-            
+        // タップされた位置の特徴点にあるARアンカーを探す
+        let hitTestResults = sceneView.hitTest(touchLocation, types: .existingPlane)
+        
         // ARアンカーがあればオブジェクトを置く
         if let hitResult = hitTestResults.first {
-            // はじめのオブジェクトは オレンジ色
+            // スタート地点のオブジェクトは オレンジ色
             // その他は 白色
             if dotNodes == [] {
                 addDot(at: hitResult, color: .systemOrange)
             } else {
                 addDot(at: hitResult, color: .white)
+                
+                /* addLine */
+                let startPosition = dotNodes[dotNodes.count-2].position
+                let endPosition = dotNodes[dotNodes.count-1].position
+                // lineノードの作成
+                let lineNode = drawLine(from: startPosition, to: endPosition)
+                sceneView.scene.rootNode.addChildNode(lineNode)
+                lineNodes.append(lineNode)
             }
         }
-            
-        
     }
     
-    func addDot(at hitResult : ARHitTestResult, color: UIColor) {
+    @IBAction private func UndoAction(_ sender: UIButton) {
+        /* remove dot */
+        if dotNodes == [] { return }
+        // sceneViewから削除
+        dotNodes.last?.removeFromParentNode()
+        // 配列から削除
+        dotNodes.removeLast()
+        
+        /* remove line */
+        if lineNodes == [] { return }
+        // sceneViewから削除
+        lineNodes.last?.removeFromParentNode()
+        // 配列から削除
+        lineNodes.removeLast()
+    }
+    
+    private func drawLine(from: SCNVector3, to: SCNVector3) -> SCNNode {
+        // 直線のGeometryを作成する
+        let source = SCNGeometrySource(vertices: [from, to])
+        let element = SCNGeometryElement(data: Data.init(bytes: [0, 1]),
+                                         primitiveType: .line,
+                                         primitiveCount: 1,
+                                         bytesPerIndex: 1)
+        let geometry = SCNGeometry(sources: [source], elements: [element])
+        
+        // 直線ノードの作成
+        let node = SCNNode()
+        node.geometry = geometry
+        node.geometry?.materials.first?.diffuse.contents = UIColor.systemOrange
+        return node
+    }
+    
+    private func addDot(at hitResult : ARHitTestResult, color: UIColor) {
+        // dotのGeometryを作成
         let dotGeometry = SCNSphere(radius: 0.01)
         let material = SCNMaterial()
         material.diffuse.contents = color
-
+        
         dotGeometry.materials = [material]
-
+        // dotノードの作成
         let dotNode = SCNNode(geometry: dotGeometry)
-
-        dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
-
+        // 座標の指定
+        dotNode.position = SCNVector3(
+            hitResult.worldTransform.columns.3.x,
+            hitResult.worldTransform.columns.3.y,
+            hitResult.worldTransform.columns.3.z)
+        // 子ノードを追加
         sceneView.scene.rootNode.addChildNode(dotNode)
-
+        // 配列に追加
         dotNodes.append(dotNode)
-
+        
         if dotNodes.count >= 2 {
             calculate()
         }
     }
-
-    func calculate (){
+    
+    private func calculate() {
         let start = dotNodes[0]
         let end = dotNodes[1]
-
-        print(start.position)
-        print(end.position)
-
+        
         let distance = sqrt(
             pow(end.position.x - start.position.x, 2) +
                 pow(end.position.y - start.position.y, 2) +
                 pow(end.position.z - start.position.z, 2)
         )
         //        distance = √ ((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
-
     }
-    
 }
 
 
@@ -107,9 +137,9 @@ extension ViewController: ARSCNViewDelegate {
             fatalError()
         }
         /*
-        // ボールオブジェクトが追加されている場合に、平面は新たに表示しない
-        if dotNodes != [] { return }
-        */
+         // ボールオブジェクトが追加されている場合に、平面は新たに表示しない
+         if dotNodes != [] { return }
+         */
         // 平面のインスタンスの生成
         let planeNode = createPlane(withPlaneAnchor: planeAnchor)
         // 小要素に追加
@@ -128,13 +158,13 @@ extension ViewController: ARSCNViewDelegate {
             return
         }
         /*
-        // ボールオブジェクトが追加されている場合に、平面オブジェクトを削除
-        if dotNodes != [] {
-            // remove node
-            node.enumerateChildNodes { (node, stop) in
-            node.removeFromParentNode() }
-        }
-        */
+         // ボールオブジェクトが追加されている場合に、平面オブジェクトを削除
+         if dotNodes != [] {
+         // remove node
+         node.enumerateChildNodes { (node, stop) in
+         node.removeFromParentNode() }
+         }
+         */
         // Geometry Update
         planeGeometry.width = CGFloat(planeAnchor.extent.x)
         planeGeometry.height = CGFloat(planeAnchor.extent.z)
@@ -143,21 +173,20 @@ extension ViewController: ARSCNViewDelegate {
     }
     
     // 平面の生成
-    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
-        
-        let plane = SCNPlane(
+    private func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
+        // planeGeometryの作成
+        let planeGeometry = SCNPlane(
             width: CGFloat(planeAnchor.extent.x),
             height: CGFloat(planeAnchor.extent.z))
         // 白　透過度0.5
-        plane.materials.first?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+        planeGeometry.materials.first?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+        // planeノードの作成
         let planeNode = SCNNode()
         
-        planeNode.geometry = plane
+        planeNode.geometry = planeGeometry
         planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
         planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
         
         return planeNode
     }
 }
-
-
